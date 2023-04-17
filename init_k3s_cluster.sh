a8e11c44bdb71622cf94907702070a87fae4d0e9
buglib <buglib@foxmail.com>
2023-04-17 12:35:05 +0800

    创建multipass实例时支持自定义重定向目录, 补建docker config目录

--- init_k3s_cluster.sh
+++ init_k3s_cluster.sh
@@ -4,10 +4,24 @@
 multipass delete master worker1 worker2
 multipass purge
 
-# 先创建三个节点
-multipass launch --name master --memory 1G --disk 5G 20.04
-multipass launch --name worker1 --memory 1G --disk 5G 20.04
-multipass launch --name worker2 --memory 1G --disk 5G 20.04
+# 创建三个节点
+# multipass launch --name master --memory 1G --disk 5G 20.04 $mount
+# multipass launch --name worker1 --memory 1G --disk 5G 20.04 $mount
+# multipass launch --name worker2 --memory 1G --disk 5G 20.04 $mount
+
+src=$1
+dst=$2
+mount=""
+if [[ $src != "" && $dst != "" ]]; then
+    mount="--mount $src:$dst"
+else
+    mount=""
+fi
+
+for host in "master" "worker1" "worker2"
+do
+    multipass launch --name $host --memory 1G --disk 5G 20.04 $mount
+done
 
 # 接着在各个节点上安装k3s
 multipass exec master -- /bin/bash -c "curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -"
@@ -24,6 +38,7 @@ multipass exec worker1 -- /bin/bash -c "sudo apt update && sudo apt install -y d
 multipass exec worker2 -- /bin/bash -c "sudo apt update && sudo apt install -y docker.io"
 # 最后，将docker镜像仓库切换为国内的
 docker_config_file=/etc/docker/daemon.json
+mkdir -p $docker_config_file
 multipass exec master -- /bin/bash -c "sudo touch ${docker_config_file} && sudo chmod 666 ${docker_config_file} && cat >> ${docker_config_file} <<EOF
 {
     "registry-mirrors": [
